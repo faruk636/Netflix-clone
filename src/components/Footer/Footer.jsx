18ee67618e09adca0c931008ab7805a0602b4e29
Faruk Ahmed <far12345ahmed@gmail.com>
2025-09-09 11:41:52 +0300

    Header and Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,58 @@
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer-outer-wrapper">
+        <div className="footer-wrapper">
+          <div className="footer-icon">
+            <FacebookOutlinedIcon/>
+            <InstagramIcon/>
+            <YouTubeIcon/>
+          </div>
+          <div className="footer-data">
+            <div>
+              <ul>
+                <li>Audio Description</li>
+                <li>Investor Relations</li>
+                <li>Legal Notice</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>Help Center</li>
+                <li>Jobs</li>
+                <li>Cookie Preferences</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>Gift Cards</li>
+                <li>Terms of Use</li>
+                <li>Corporate Information</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>Media Center</li>
+                <li>Privacy</li>
+                <li>Contact Us</li>
+              </ul>
+            </div>
+          </div>
+          <div className="service">
+            <p>Service Code</p>
+          </div>
+          <div className="copy-right">
+            &copy;1997-2025 Netflix, Inc.
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,48 @@
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import NetflixLogo from '../../assets/images/NetflixLogo.svg'
+import './header.css'
+
+const Header = () => {
+  return (
+    <>
+      <div className="header-outer-wrapper">
+        <div className="header-wrapper">
+          <div className="header-left-side">
+              <ul>
+                <li>
+                  <img src={NetflixLogo} alt="" />
+                </li>
+                <li>Home</li>
+                <li>TVShows</li>
+                <li>Movies</li>
+                <li>Latest</li>
+                <li>MyList</li>
+                <li>Browse by language</li>
+              </ul>
+          </div>
+          <div className="header-right-side">
+            <ul>
+              <li>
+                <SearchIcon />
+              </li>
+              <li>
+                <NotificationsNoneIcon />
+              </li>
+              <li>
+                <AccountBoxIcon />
+              </li>
+              <li>
+                <ArrowDropDownIcon />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
